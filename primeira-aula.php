de273cc02287d0b6f29ebac8b6d46bb258a099cc
Isaias <isaiasbatistapy@gmail.com>
2024-04-04 11:06:50 -0300

    primeira aula ajuste

--- primeira-aula.php
+++ primeira-aula.php
@@ -3,6 +3,7 @@
 require "vendor/autoload.php";
 use app\src\class\AtividadePessoa;
 use app\src\class\Pessoa;
+use app\src\class\UploadFoto;
 
 $pessoa = new Pessoa;
 $pessoa->idade = 45;
@@ -22,9 +23,9 @@ $atividade = new AtividadePessoa;
 echo $atividade->andar() .' <br>';
 echo $atividade->pular();
 
-$upload = new  \app\src\class\UploadFoto;
+$upload = new  UploadFoto('primeirafoto.png');
 
-$upload->file('Imagemisaias.png');
+
 $upload->extension();
 $upload->rename();
 $upload->upload();
