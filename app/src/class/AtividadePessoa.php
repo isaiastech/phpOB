40e6740b3226f13a63e01a85552a73bb6dec58a8
Isaias <isaiasbatistapy@gmail.com>
2024-04-04 10:48:38 -0300

    criando namespace

--- app/src/class/AtividadePessoa.php
+++ app/src/class/AtividadePessoa.php
@@ -1,6 +1,6 @@
 <?php
 
-
+namespace app\src\class;
 
 class AtividadePessoa
 {
--- index.php
+++ index.php
@@ -1,26 +1,26 @@
 <?php
 
 require "vendor/autoload.php";
+use app\src\class\AtividadePessoa;
+use app\src\class\Pessoa;
 
+$pessoa = new Pessoa;
+$pessoa->idade = 45;
+$pessoa->nome = "Isaias";
+$pessoa->email = "isaias@isaiastech.com.br";
 
-// $pessoa = new Pessoa;
+echo $pessoa->dados() .' <br>';
 
-// $pessoa->idade = 45;
-// $pessoa->nome = "Isaias";
-// $pessoa->email = "isaias@isaiastech.com.br";
+$pessoa->idade = 36;
+$pessoa->nome = "Pedro";
+$pessoa->email = "pedro@isaiastech.com.br";
 
-// echo $pessoa->dados() .' <br>';
+echo $pessoa->dados() .' <br>';
 
-// $pessoa->idade = 36;
-// $pessoa->nome = "Pedro";
-// $pessoa->email = "pedro@isaiastech.com.br";
+$atividade = new AtividadePessoa;
 
-// echo $pessoa->dados() .' <br>';
-
-// $atividade = new AtividadePessoa;
-
-// echo $atividade->andar() .' <br>';
-// echo $atividade->pular();
+echo $atividade->andar() .' <br>';
+echo $atividade->pular();
 
 $upload = new  \app\src\class\UploadFoto;
 
