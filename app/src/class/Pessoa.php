5db487d116f2f760fd2b931a23efa459d648bbe0
Isaias <isaiasbatistapy@gmail.com>
2024-04-04 10:44:38 -0300

    trabalhando com auload

---
+++ app/src/class/Pessoa.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\src;
+
+
+class Pessoa
+{
+    public $idade;
+    public $nome;
+    public $email;
+
+    public function dados()
+    {
+        return "Meu nome é {$this->nome}, minha idade é {$this->idade} e meu email é {$this->email}";
+    }
+}
+
---
+++ app/src/class/UploadFoto.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace app\src\class;
+
+class UploadFoto
+{
+    public $file;
+    public $newName;
+    public $extensions = ['png', 'jpg'];
+
+    public function file($file)
+    {
+        $this->file = $file;
+    }
+
+    public function extension()
+    {
+      return pathinfo($this->file,  PATHINFO_EXTENSION);   
+    }
+
+    public function rename()
+    {
+        $uniqId = uniqid(true);
+        $this->newName = $uniqId . '.' . $this->extension();
+
+    }
+
+    public function upload()
+    {
+        return $this->newName;
+    }
+    
+}
