102e580635b3b316e688a5a9ace6f0541b89c2ea
Isaias <isaiasbatistapy@gmail.com>
2024-04-05 12:46:56 -0300

    Modificando código

--- app/src/class/Upload.php
+++ app/src/class/Upload.php
@@ -8,12 +8,8 @@ class Upload
 
     public function teste()
     {
-<<<<<<< HEAD
         return "Teste";
     }
 
-=======
-        return "teste";
-    }
->>>>>>> a04e5a7628272c4bb5052968d6abe2221039a755
 }
+
--- index.php
+++ index.php
@@ -3,16 +3,6 @@ require "vendor/autoload.php";
 
 use app\src\class\UploadFoto;
 
-$upload = new UploadFoto('teste.png');
-
-<<<<<<< HEAD
-
-$upload = new  UploadFoto('foto dois.jpg');
-
-
-echo $upload->teste();
-
-=======
-echo $upload->teste();
->>>>>>> a04e5a7628272c4bb5052968d6abe2221039a755
+$upload = new UploadFoto('Isaias.png');
+$upload->teste();
 
